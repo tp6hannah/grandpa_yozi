48f39be2d7788c94b29922d23c879e483c307e2a
tp6hannah <hannah51125@gmail.com>
2018-06-04 20:58:03 +0800

    greeting text

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+
+from __future__ import unicode_literals
+
+import errno
+import os
+import sys
+import tempfile
+from argparse import ArgumentParser
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,7 +16,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URITemplateAction,
+    PostbackTemplateAction, DatetimePickerTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -42,6 +59,34 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         message)
+
+
+# greeting text and image
+@handler.add(FollowEvent)
+def handle_follow_message(event):
+    reply_arr=[]
+    firstM = TextSendMessage(text="這是東伯的柚子園")
+    secondM = TextSendMessage(text="Yes, this is me!")
+    pic_url ='https://media.licdn.com/dms/image/C5103AQGFkQP0UXEFLA/profile-displayphoto-shrink_200_200/0?e=1529672400&v=beta&t=_tGqGLrPJ856JRyxZ-f9zhBGny5iWUSIQqxQ5N5hKsQ'
+    image_message = ImageSendMessage(
+        original_content_url= pic_url,
+        preview_image_url= pic_url
+    )
+
+    confirm_template = ConfirmTemplate(text='Want to know more about me?', actions=[
+            MessageTemplateAction(label='Yes!', text='About me'),
+            MessageTemplateAction(label='No!', text='No'),
+    ])
+    template_message = TemplateSendMessage(
+        alt_text='Confirm', template=confirm_template)
+
+    reply_arr.append(firstM)
+    reply_arr.append(image_message)
+    reply_arr.append(secondM)
+    reply_arr.append(template_message)    
+
+    line_bot_api.reply_message(event.reply_token , reply_arr)
+
 
 import os
 if __name__ == "__main__":
